42395a832b57a51744df9ba8c68f310148f7f1cf
Ulices Gonzalez <sgonzalez30@sfsu.edu>
2024-10-22 13:42:54 -0700

    created files for vcb and directory entry types with current design for both

---
+++ fsDirEnt.h
@@ -0,0 +1,27 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2024
+* Name:: Ulices Gonzalez, Marco Robles, Yash Pachori, Prashrit Magar
+* Student IDs:: 923328897, 921282632, 923043313, 922068027
+* GitHub-Name:: csc415-filesystem-ulicessgg
+* Group-Name:: The Gunners
+*
+* File:: fsDirEnt.h
+*
+* Description:: creates the directory entry struct
+*
+**************************************************************/
+
+#ifndef time_t
+typedef time_t;
+#endif
+
+typedef struct dir_Entry 
+{
+    time_t creation_date; // Saves the creation timestamp of the entry, 8 bytes
+    time_t last_edited; // When edited saves the timestamp, 8 bytes
+    time_t last_accessed; // When accessed saves the timestamp, 8 bytes
+    unsigned int blockPos; // Holds the index of the position in blocks, 4 bytes
+    unsigned int size; // Stores the size in bytes of the file, 4 bytes
+    bool is_Directory; // Differentiates if the entry is a new directory or a new file, 1 byte
+    char name[63]; // Name of the file or directory being created, 63 bytes
+ } dir_Entry;
---
+++ fsVCB.h
@@ -0,0 +1,28 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2024
+* Name:: Ulices Gonzalez, Marco Robles, Yash Pachori, Prashrit Magar
+* Student IDs:: 923328897, 921282632, 923043313, 922068027
+* GitHub-Name:: csc415-filesystem-ulicessgg
+* Group-Name:: The Gunners
+*
+* File:: fsVCB.h
+*
+* Description:: creates the volume control block struct
+*
+**************************************************************/
+
+#ifndef uint64_t
+typedef uint64_t;
+#endif
+
+typedef struct vcb
+{
+   uint64_t totalBlocks; // Total number of blocks in the volume // 8 bytes
+   uint64_t blockSize; // Size of each block in bytes // 8 bytes
+   uint64_t freeBlockCount; // Where total free space is counted // 8 bytes
+   uint64_t freeBlockStart; // Index for the beginning of the free space in volume // 8 bytes
+   uint64_t fatSize; // size of the file allocation table // 8 bytes
+   uint64_t rootLoc; // Index for finding the root directory // 8 bytes
+   unsigned long volumeSignature;// Signature to identify the volume // 8 bytes
+   char sysType[8]; // Holds the type of volume/file system // 8 bytes
+} vcb;
